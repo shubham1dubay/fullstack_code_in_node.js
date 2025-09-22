a7689331e69882df488695e726463957167cb505
Shubham dubey <shubham.dubey@oodles.io>
2025-09-22 15:02:34 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import dotenv from "dotenv";
+import express from "express";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      titel: "joke1",
+      content: "this is joke1",
+    },
+    {
+      id: 2,
+      titel: "joke2",
+      content: "this is joke2",
+    },
+    {
+      id: 3,
+      titel: "joke3",
+      content: "this is joke3",
+    },
+    {
+      id: 4,
+      titel: "joke4",
+      content: "this is joke4",
+    },
+    {
+      id: 5,
+      titel: "joke5",
+      content: "this is joke5",
+    },
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
