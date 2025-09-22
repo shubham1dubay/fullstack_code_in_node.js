12805fb94eaa53d956eaf3829be5ee89fa4163de
Shubham dubey <shubham.dubey@oodles.io>
2025-09-22 16:55:46 +0530

    forntend

---
+++ forntend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/jokes")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  return (
+    <>
+      <h1>Full Stack</h1>
+      <p>JOKES: {jokes.length}</p>
+      {jokes.map((joke) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default App;
